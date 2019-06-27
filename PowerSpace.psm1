c636724ed16d5535f24a8c3b71a0dadbbd620e17
Lukas Voeller <voeller.lukas@gmail.com>
2019-06-27 17:44:16 +0200

    Moveable Spaceship!

--- PowerSpace.psm1
+++ PowerSpace.psm1
@@ -9,8 +9,8 @@ class Star {
 
 class Spaceship {
     [string]$name = "Spaceshell"
-    [int]$x = 0
-    [int]$y = 0
+    [int]$x = 10
+    [int]$y = 10
 
     [string]$appearance1 = " ____"
     [string]$appearance2 = "O    |="
@@ -45,19 +45,52 @@ function Start-PowerSpace {
         $starline = ""
     
         # Handeling the lines without a star
-        if ($row -eq 5 -and $star.pos -le $spaceship.appearance1.Length) {
-            $starline = $starline + $spaceship.appearance1
+        if ($row -eq $spaceship.y){                                                                                         # determines that we are in a spaceship row
+            for ($i = 0; $i -lt $spaceship.appearance1.Length; $i++) {
+                if ($star.pos -eq ($spaceship.x + $i)) {                                                                    # star is behind the ship
+                    for ($i = 0; $i -lt $spaceship.x; $i++) {
+                        if ($debug) {
+                            $starline = $starline + "D"
+                        } else {
+                            $starline = $starline + " "
+                        }
+                    }
 
-            for ($i = 0; $i -lt ($windowWidth - $spaceship.appearance1.Length); $i++) {
-                if ($debug) {
-                    $starline = $starline + "-"
-                }else {
-                    $starline = $starline + " "
+                    $starline = $starline + $spaceship.appearance1
+
+                    for ($i = (($spaceship.x + $spaceship.appearance1.Length)); $i -lt $windowWidth; $i++) {
+                        if ($debug) {
+                            $starline = $starline + "D"
+                        } else {
+                            $starline = $starline + " "
+                        }
+                    }
+    
+                    return $starline
                 }
             }
 
-            return $starline
+            if ($star.pos -lt 1) {                                                                                          # star is out of bounds
+                for ($i = 0; $i -lt $spaceship.x; $i++) {
+                    if ($debug) {
+                        $starline = $starline + "W"
+                    } else {
+                        $starline = $starline + " "
+                    }
+                }
 
+                $starline = $starline + $spaceship.appearance1
+
+                for ($i = (($spaceship.x + $spaceship.appearance1.Length)); $i -lt $windowWidth; $i++) {
+                    if ($debug) {
+                        $starline = $starline + "W"
+                    } else {
+                        $starline = $starline + " "
+                    }
+                }
+
+                return $starline
+            }
         } elseif ($star.pos -lt 1) {
             for ($i = 0; $i -lt $windowWidth; $i++) {
                 if ($debug) {
@@ -70,21 +103,66 @@ function Start-PowerSpace {
             return $starline
         }
         
-        # Handeling the space before a star and adding the star
-        if ($row -eq 5 -and $star.pos -gt $spaceship.appearance1.Length) {
-            $starline = $starline + $spaceship.appearance1
+        # Main Handle
+        if ($row -eq $spaceship.y){                                                                             # determines that we are in a spaceship row
+            if ($star.pos -lt $spaceship.x) {                                                                   # the star is behind the ship
+                for ($i = 1; $i -lt $star.pos; $i++) {
+                    if ($debug) {
+                        $starline = $starline + "L"
+                    }else {
+                        $starline = $starline + " "
+                    }
+                }
+    
+                $starline = $starline + $star.appearance
 
-            for ($i = 1; $i -lt ($star.pos - $spaceship.appearance1.Length); $i++) {
-                if ($debug) {
-                    $starline = $starline + "+"
-                } else {
-                    $starline = $starline + " "
+                for ($i = $star.pos; $i -lt $spaceship.x; $i++) {
+                    if ($debug) {
+                        $starline = $starline + "L"
+                    } else {
+                        $starline = $starline + " "
+                    }
+                }
+
+                $starline = $starline + $spaceship.appearance1
+
+                for ($i = ($spaceship.x + $spaceship.appearance1.Length); $i -lt $windowWidth; $i++) {
+                    if ($debug) {
+                        $starline = $starline + "L"
+                    } else {
+                        $starline = $starline + " "
+                    }
+                }
+            } elseif ($star.pos -ge ($spaceship.x + $spaceship.appearance1.Length)) {                           # the star is in front of the ship
+                for ($i = 0; $i -lt $spaceship.x; $i++) {
+                    if ($debug) {
+                        $starline = $starline + "R"
+                    } else {
+                        $starline = $starline + " "
+                    }
+                }
+
+                $starline = $starline + $spaceship.appearance1
+
+                for ($i = (($spaceship.x + $spaceship.appearance1.Length)); $i -lt $star.pos; $i++) {
+                    if ($debug) {
+                        $starline = $starline + "R"
+                    } else {
+                        $starline = $starline + " "
+                    }
+                }
+
+                $starline = $starline + $star.appearance
+
+                for ($i = $star.pos; $i -lt ($windowWidth - 1); $i++) {
+                    if ($debug) {
+                        $starline = $starline + "R"
+                    } else {
+                        $starline = $starline + " "
+                    }
                 }
             }
-
-            $starline = $starline + $star.appearance
-
-        } else {
+        } else {                                                                                                # for all other lines without the spaceship
             for ($i = 1; $i -lt $star.pos; $i++) {
                 if ($debug) {
                     $starline = $starline + "+"
@@ -95,13 +173,16 @@ function Start-PowerSpace {
 
             $starline = $starline + $star.appearance
         }
+    
         
-        # Handling the space after thr star
-        for ($i = $star.pos; $i -lt $windowWidth; $i++) {
-            if ($debug) {
-                $starline = $starline + "-"
-            } else {
-                $starline = $starline + " "
+        # Handling the space after a star. Only for non-spaceship line and stars in fornt of the ship
+        if ($row -ne $spaceship.y) {
+            for ($i = $star.pos; $i -lt $windowWidth; $i++) {
+                if ($debug) {
+                    $starline = $starline + "-"
+                } else {
+                    $starline = $starline + " "
+                }
             }
         }
     
@@ -164,6 +245,7 @@ function Start-PowerSpace {
 
             $starfield = makeStarfield $stars $spaceship                # Update
             Write-Host $starfield                                       # Draw
+            #Write-Host " "
             moveStarfield($stars)                                       # Move
 
             #Write-Host "X:"$windowWidth "Y:"$windowHeight "Stars:"$stars.Length -NoNewline
